190258042add02e78018a38d60809e1a0eddd0fc
Everton Souza <everton.souzaa@outlook.com.br>
2022-11-20 18:26:35 -0300

    Exercicios

---
+++ lista-exercicios-python-brasil/1.estrurura-sequencial.py
@@ -0,0 +1,12 @@
+# 1.Faça um Programa que mostre a mensagem "Alô mundo" na tela. 
+print('Alô Mundo!!!')
+
+# 2.Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+num = int(input('Digite um numero: '))
+print('O numero informado foi', num)
+
+# 3.Faça um Programa que peça dois números e imprima a soma.
+num_1 = int(input('Digite um numero: '))
+num_2 = int(input('Digite um numero: '))
+soma = num_1 + num_2
+print(f'\nA soma entre os numeros e: {soma}')
