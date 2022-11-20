58d5f56b1fc700f33bde89a2a69b6f2ce3b4f082
Everton Souza <everton.souzaa@outlook.com.br>
2022-11-20 16:43:25 -0300

    Update exercicios.py

--- lista-exercicios-python-brasil/1.estrurura-sequencial/exercicios.py
+++ lista-exercicios-python-brasil/1.estrurura-sequencial/exercicios.py
@@ -1,5 +1,6 @@
 # 1.Faça um Programa que mostre a mensagem "Alô mundo" na tela. 
 print('Alô Mundo!!!')
+
 # 2.Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 num = int(input('Digite um numero: '))
 print('O numero informado foi', num)
