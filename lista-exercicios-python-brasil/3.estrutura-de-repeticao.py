4a8bb2e8d88af6ba304a9156cf980ca60898fd19
Everton Souza <everton.souzaa@outlook.com.br>
2022-11-25 19:17:46 -0300

    Update 3.estrutura-de-repeticao.py

--- lista-exercicios-python-brasil/3.estrutura-de-repeticao.py
+++ lista-exercicios-python-brasil/3.estrutura-de-repeticao.py
@@ -17,3 +17,30 @@ while True:
     else:
         print('Cadastro concluído!')
         break
+# Faça um programa que leia e valide as seguintes informações:
+    #Nome: maior que 3 caracteres;
+    #Idade: entre 0 e 150;
+    #Salário: maior que zero;
+    #Sexo: 'f' ou 'm';
+    #Estado Civil: 's', 'c', 'v', 'd';
+nome = input('Digite um nome: ')
+idade = int(input('Digite uma idade: '))
+salario = float(input('Digite seu sálario: '))
+sexo = input('Digite seu sexo: ')
+estado_civil = input('Digite seu estado civil: ')
+
+if len(nome) <= 3:
+    print('Erro! Nome deve ser maior que 3 caracteres.')
+else:
+    if idade < 0 or idade > 150:
+        print('Erro! Idade deve está entre 0 e 150.')
+    else:
+        if salario <0:
+            print('Erro! Salário deve ser maior que 0.')
+        else:
+            if sexo != 'f' and sexo != 'm':
+                print('Erro! Sexo deve ser f ou m.')
+            else:
+                if estado_civil != 's' and estado_civil != 'c' and estado_civil != 'v' and estado_civil != 'd':
+                    print("Erro! Estado civil deve ser: 's', 'c', 'v', 'd'")
+                
