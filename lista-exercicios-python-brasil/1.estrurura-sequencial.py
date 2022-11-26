d7ef5346bb4697598caa40a849a6d94f83a60fba
Everton Souza <everton.souzaa@outlook.com.br>
2022-11-26 08:24:51 -0300

    Exercicios

--- lista-exercicios-python-brasil/1.estrurura-sequencial.py
+++ lista-exercicios-python-brasil/1.estrurura-sequencial.py
@@ -12,7 +12,6 @@ soma = num_1 + num_2
 print(f'\nA soma entre os numeros e: {soma}')
 
 # 4.Faça um Programa que peça as 4 notas bimestrais e mostre a média.
-print('Notas bimestrais\n')
 nota_1 = int(input('Digite sua nota do 1° bimestre: '))
 nota_2 = int(input('Digite sua nota do 2° bimestre: '))
 nota_3 = int(input('Digite sua nota do 3° bimestre: '))
@@ -22,14 +21,12 @@ media = (nota_1 + nota_2 + nota_3 + nota_4)/4
 print(f'\nSua nota média bimestral é: {media}')
 
 # 5. Faça um Programa que converta metros para centímetros.
-print('Conversor de metros para centímetros\n')
 metro = int(input('Digite um número em metros: '))
 centimetro = metro * 100
 
 print(f'O valor convertido é: {centimetro} centímetros.')
 
 # 6.Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
-print('Calculo de área circular\n')
 raio = int(input('Digite o raio do circulo: '))
 area = 2.14 * raio**2
 
