199134a5f4783eda49f3b4ee5a57dbd16f116e39
Everton Souza <everton.souzaa@outlook.com.br>
2022-11-27 23:15:50 -0400

    Update 3.estrutura-de-repeticao.py

--- lista-exercicios-python-brasil/3.estrutura-de-repeticao.py
+++ lista-exercicios-python-brasil/3.estrutura-de-repeticao.py
@@ -53,3 +53,5 @@ while True:
     if pais_a >= pais_b:
         print(ano)
         break
+
+# 5.Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operação.
