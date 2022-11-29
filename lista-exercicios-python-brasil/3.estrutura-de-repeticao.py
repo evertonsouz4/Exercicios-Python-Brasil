ab1758880bb86a08b8abc0f545fb74e4eaed5cee
Everton Souza <everton.souzaa@outlook.com.br>
2022-11-29 06:04:56 -0400

    Update 3.estrutura-de-repeticao.py

--- lista-exercicios-python-brasil/3.estrutura-de-repeticao.py
+++ lista-exercicios-python-brasil/3.estrutura-de-repeticao.py
@@ -55,18 +55,27 @@ while True:
         break
 
 # 5.Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operação.
+
 while True:
-    ano = 0
-
     pais_a = int(input('Digite a população do pais a: '))
-    tx_pais_a = float(input('Digite a taxa de crescimento do pais a: '))
-
     pais_b = int(input('Digite a população do pais b: '))
+    tx_pais_a = float(input('Digite a taxa de crescimento do pais a: '))
     tx_pais_b = float(input('Digite a taxa de crescimento do pais b: '))
-
+    # validação da entrada.
     if tx_pais_a < 0:
-        print('Erro! digite um valor positivo.')
+        print('Erro! digite um valor positiva.')
         continue
     elif tx_pais_b < 0:
-        print('Erro! digite um valor positivo.')
+        print('Erro! digite um valor positiva.')
         continue
+
+    break
+
+ano = 0  
+while True:
+    ano = ano + 1
+    pais_a = pais_a + pais_a * tx_pais_a
+    pais_b = pais_b + pais_b * tx_pais_b
+    if pais_a >= pais_b:
+        print(ano)
+        break
